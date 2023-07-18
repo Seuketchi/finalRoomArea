f9594137d2a8ff863327df5b3aca5aeada00c1ba
Seuketchup <123664244+Seuketchup@users.noreply.github.com>
2023-07-18 09:41:00 +0800

    Implement final method

---
+++ src/LivingRoom.java
@@ -0,0 +1,7 @@
+public class LivingRoom extends Room{
+    // The constructor simply calls the parent's constructor using super()
+    public LivingRoom(double width, double height){
+        super(width,height);
+    }
+    // Not allowed to override getArea() here
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        LivingRoom myLivingRoom = new LivingRoom(5,3);
+        double area = myLivingRoom.getArea();
+        System.out.println(area);
+    }
+}
